f2d25e1a7c7d6fb08fe68626c72c76cd873715f9
michinobu.sekikawa <abc_0615@icloud.com>
2019-07-05 03:33:39 +0900

    section3:入力フォームを追加

--- app.rb
+++ app.rb
@@ -5,3 +5,16 @@ get '/' do
     @now = Time.now
     erb :index
 end
+
+get '/contact_new' do
+    erb :contact_form
+end
+
+post '/contacts' do
+    puts '###　送信されだデータ ###'
+    p params
+    
+    # DBに保存
+    
+    redirect '/'
+end
